21b0abe22fce0cd22cfd0f421389501459d7caf4
Frank Wagner <frank.wagner@fr.ibm.com>
2022-11-25 15:10:59 +0100

    better name: declaring source name

--- diesel-i18n/src/main/scala/diesel/i18n/MessageKey.scala
+++ diesel-i18n/src/main/scala/diesel/i18n/MessageKey.scala
@@ -1,19 +1,21 @@
 package diesel.i18n
 
-case class MessageKey(key: String)
+case class DeclaringSourceName(name: String)
 
-object MessageKey {
+object DeclaringSourceName {
   import scala.language.experimental.macros
 
-  implicit def messageKey: MessageKey = macro messageKeyImpl
+  implicit def declaringSourceName: DeclaringSourceName = macro declaringSourceNameImpl
 
   private type Context = scala.reflect.macros.blackbox.Context
-  def messageKeyImpl(c: Context): c.Expr[MessageKey] = {
+  def declaringSourceNameImpl(c: Context): c.Expr[DeclaringSourceName] = {
     import c.universe._
-    var owner      = enclosingOwner(c)
-    while (owner.isSynthetic) owner = owner.owner
-    val simpleName = owner.name.decodedName.toString().trim()
-    c.Expr[MessageKey](q"""${c.prefix}($simpleName)""")
+    var owner = enclosingOwner(c)
+    while (owner.isSynthetic) {
+      owner = owner.owner
+    }
+    val name  = owner.name.decodedName.toString().trim()
+    c.Expr[DeclaringSourceName](q"""${c.prefix}($name)""")
   }
 
   private def enclosingOwner(c: Context) = c.internal.enclosingOwner
